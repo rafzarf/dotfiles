ce44a28adb4fb08c8ec93130620310679374d369
rafzarf <xxrafzaxx@gmail.com>
2024-08-01 20:12:24 +0700

    deleting duplicated .config folder

--- .config/.config/nvim/lua/plugins/vim-ros-ycm.lua
+++
@@ -1,9 +0,0 @@
--- ~/.config/nvim/lua/user/plugin/vim-ros-ycm.lua
-
-return {
-  "kgreenek/vim-ros-ycm",
-  event = "BufRead", -- Optional: Load the plugin when a file is read
-  config = function()
-    -- Additional configuration if needed
-  end,
-}
